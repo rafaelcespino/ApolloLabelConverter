c67bf2d78828853718b194d361edc0ba4fee1074
rafaelcespino <55030277+rafaelcespino@users.noreply.github.com>
2021-02-14 16:42:02 -0600

    Add files via upload

--- labelConverter.py
+++ labelConverter.py
@@ -2,37 +2,32 @@ import numpy as np
 import os 
 import scipy.io as sio
 
-labels = "./SAD/train/FS02_train_001.txt"
-outpath = "./FS02_train_001.mat"
+inputDir = "./SAD/dev/"
 
+for labelFile in os.listdir(inputDir):
+    with open("./SAD/dev/{}".format(labelFile)) as f:
+        lines = f.readlines()
+        base = os.path.basename(labelFile)
+        base = os.path.splitext(base)[0]
+    print(base)
 
-#read each line of file into list
-with open(labels) as f:
-    lines = f.readlines()
+    lastLine = lines[-1]
+    totalTime = lastLine.split()[3]
+    arraySize = float(totalTime) * 16000
+    arraySize = int(arraySize)
+    matLabels = np.zeros(arraySize)
 
-#initialize 2d numpy array with zeros 
-lastLine = lines[-1]
-totalTime = lastLine.split()[3]
-arraySize = float(totalTime) * 16000
-arraySize = int(arraySize)
-matLabels = np.zeros(arraySize)
+    for line in lines:
+        label = line.split()
+        if label[4] == "S":
 
-
-
-#obtain each line with speech data 
-for line in lines:
-    label = line.split()
-    if label[4] == "S":
-
-        start = float(label[2]) * 16000
-        start = int(start)
+            start = float(label[2]) * 16000
+            start = int(start)
         
-        end = float(label[3]) * 16000
-        end = int(end)
+            end = float(label[3]) * 16000
+            end = int(end)
 
-        matLabels[start:end] = 1
-
-#reshape array into a 2d array and save to a mat file
-matLabels = matLabels.reshape((arraySize, 1))
-print(matLabels)
-sio.savemat(outpath, {"y_label":matLabels})
+    matLabels[start:end] = 1
+    matLabels = matLabels.reshape((arraySize, 1))
+    sio.savemat("./convertedLabels/{}.mat".format(base), {"y_label":matLabels})
+        
